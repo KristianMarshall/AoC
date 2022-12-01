fdb756f16fdb9ae02f227819ffb8f4fe59da172a
KristianMarshall <113186609+KristianMarshall@users.noreply.github.com>
2022-12-01 09:01:42 -0500

    Got more of the structure done. Finished Day 1 :)

---
+++ public/days/day1.js
@@ -0,0 +1,40 @@
+fetch("/input/day1/part1.txt")
+.then(response => response.text())
+.then(result => {
+    printOutput("Fetch Completed. Processing Data...");
+
+    elfFoodArray = result.split('\n');
+    let elfArray = [];
+    let totalCal = 0;
+    elfFoodArray.forEach(elfFood => {
+        
+        if(elfFood != '')
+            totalCal += parseInt(elfFood);
+        else{
+            elfArray.push(totalCal);
+            totalCal = 0;
+        }
+
+    });
+
+    printOutput("Elves calories counted. Now finding the largest by sorting array...");
+    elfArray.sort(function(a, b){return a - b});
+    elfArray.reverse();
+
+    printOutput();
+    printOutput(`The elf with the most calories is carrying <span id="answer">${elfArray[0]}</span> calories`);
+
+    document.querySelector("#p2").insertAdjacentHTML("afterend", "<p>Finding the three largest calorie counts...</p>");
+
+    let topThree = 0;
+    for (let elf = 0; elf < 3; elf++)
+        topThree += elfArray[elf];
+
+    printOutput(`The top three elves are carrying <span id="answer">${topThree}</span> calories`);
+});
+
+function printOutput(stringToPrint = ""){
+    let output = document.querySelectorAll("p");
+    stringToPrint = `<p>${stringToPrint}</p>`;
+    output[output.length-1].insertAdjacentHTML("afterend", stringToPrint);
+}
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 app.use(express.static("public"));
-app.listen(3000);
+app.listen(3001);
 app.set('view engine', 'ejs');
 
 app.get("/", (request, response) => {
     response.render("index");
+});
+
+app.get("/day/:day", (request, response) => {
+    response.render(`day`, {day: request.params.day});
 });
